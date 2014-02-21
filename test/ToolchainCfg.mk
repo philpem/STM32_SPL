d863d06c1b638a90b592631eae24a627cd277da2
Philip Pemberton <philpem@philpem.me.uk>
2014-02-21 12:11:06 +0000

    Add STM32F10x template files, update test/ToolchainCfg.mk for Cortex-M0

--- test/ToolchainCfg.mk
+++ test/ToolchainCfg.mk
@@ -7,13 +7,20 @@ RANLIB	:=	$(CROSS)ranlib
 OBJDUMP	:=	$(CROSS)objdump
 OBJCOPY	:=	$(CROSS)objcopy
 
+# Assume Cortex M3 to start with
+CFLAGS	:=	-mthumb -mcpu=cortex-m3
+
+# Cortex-M0 devices
+ifneq ($(filter STM32F030 STM32F031 STM32F051 STM32F072 STM32F042 STM23F0XX_MD STM32F0XX_LD STM32F0XX_HD STM32F030X8 STM32F030X6,$(PLATFORM)),)
+  CFLAGS	:=	-mthumb -mcpu=cortex-m0
+endif
+
+# Cortex-M4F devices
 ifneq ($(filter STM32F40_41xxx STM32F427_437xx STM32F429_439xx STM32F401xx,$(PLATFORM)),)
   # STM32F4 (Cortex M4F) has hardware floating point
   CFLAGS	:=	-mthumb -mcpu=cortex-m4 -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -fsingle-precision-constant -D__FPU_PRESENT=1 -D__FPU_USED=1
 else
-  CFLAGS	:=	-mthumb -mcpu=cortex-m3
 endif
-#-DSTM32F10X_MD_VL -DUSE_STDPERIPH_DRIVER
 
 # to write to flash then run
 LDFLAGS+=-Wl,-T,src/stm32_flash.ld
